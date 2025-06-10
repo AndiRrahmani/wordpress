177b9c6753212d605152f729ce91aa43313b4cb7
Andi Rrahmani <andirrahmani28@gmail.com>
2025-06-10 07:06:26 -0700

    module 3

--- functions.php
+++ functions.php
@@ -7,10 +7,40 @@ function load_scripts(){
         array(),
         filemtime(get_template_directory().'/style.css'),'all'
     );
+
+    wp_enqueue_script('dropdown',get_template_directory_uri().'/js/dropdown.js',array(),'1.0',false);
+
 }
 
 add_action('wp_enenqueue_scripts','load_scripts');
 
+function config() {
+    register_nav_menus(
+        array(
+            'wp_devs_main_menu' => 'Main Menu',
+            'wp_devs_footer_menu' => 'Footer Menu',
+        )
+        );
+        $args = array(
+            'height' =>225,
+            'width' => 1920,
+        );
 
+        
+        add_theme_support('custom-header', $args);
+        add_theme_support('post_thumbnails');
+        add_theme_support('custom-logo', array(
+            'height' => 200,
+            'width' => 110,
+            'flex-height' => true,
+            'flex-width' => true,
+        ));
+        add_theme_support('automatic-feed-links');
+        add_theme_support('html5',array('search-form','comment-form','comment-list','gallery','caption','style','script'));
+
+        ));
+
+        add_action('after_setup_theme','config',0);
+    }
 
 ?>
